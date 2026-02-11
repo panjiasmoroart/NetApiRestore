c8378b0329c6b99bb9c354e4d04356290d6210bc
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-02-11 12:24:56 +0700

    API Basics - Creating the Entity Framework DbContext Class

---
+++ NetApiRestore/Data/StoreContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetApiRestore.Entities;
+
+namespace NetApiRestore.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+	{
+		public required DbSet<Product> Products { get; set; }
+	}
+}
--- NetApiRestore/Program.cs
+++ NetApiRestore/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using NetApiRestore.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+	opt.UseMySql(
+		connectionString,
+		ServerVersion.AutoDetect(connectionString) 
+	);
+});
+
 
 var app = builder.Build();
 
