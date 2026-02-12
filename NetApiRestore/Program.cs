1cf24ffefa8860f18aed14dd66571e1b095693bc
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-02-12 21:41:28 +0700

    Adding CORS - For frontend consumption

--- NetApiRestore/Program.cs
+++ NetApiRestore/Program.cs
@@ -11,11 +11,16 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
 	opt.UseMySql(connectionString, mysqlServerVersion);
 });
+builder.Services.AddCors();
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt =>
+{
+	opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
 
 //app.UseHttpsRedirection();
 
