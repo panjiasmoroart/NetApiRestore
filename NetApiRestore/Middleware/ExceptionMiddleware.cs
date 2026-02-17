cd283a81e1c5019062eb6554c1dfd54e3f329d3c
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-02-17 19:38:21 +0700

    Error Handling - Adding Exception Handling Middleware Part 1

---
+++ NetApiRestore/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace NetApiRestore.Middleware
+{
+	public class ExceptionMiddleware(IHostEnvironment env, ILogger<ExceptionMiddleware> logger) : IMiddleware
+	{
+		public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+		{
+			try
+			{
+				await next(context);
+			}
+			catch (Exception ex)
+			{
+				await HandleException(context, ex);
+			}
+		}
+
+		private async Task HandleException(HttpContext context, Exception ex)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
