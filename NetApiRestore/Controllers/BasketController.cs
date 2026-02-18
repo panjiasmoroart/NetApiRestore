9433da1f981207ae76d67d71623a447c78e49703
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-02-18 21:54:07 +0700

    Adding Shopping Cart Feature - Creating a basket controller

---
+++ NetApiRestore/Controllers/BasketController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetApiRestore.Data;
+using NetApiRestore.Entities;
+
+namespace NetApiRestore.Controllers
+{
+	public class BasketController(StoreContext context) : BaseApiController
+	{
+		[HttpGet]
+		public async Task<ActionResult<Basket>> GetBasket()
+		{
+			var basket = await context.Baskets
+				.Include(x => x.Items)
+				.ThenInclude(x => x.Product)
+				.FirstOrDefaultAsync(x => x.BasketId == Request.Cookies["basketId"]);
+
+			if (basket == null) return NoContent();
+
+			return basket;
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+		{
+			// get bakset 
+			// create basket   
+			// get product 
+			// add item to basket 
+			// save changes
+
+			return StatusCode(201);
+		}
+
+		[HttpDelete]
+	}
+}
