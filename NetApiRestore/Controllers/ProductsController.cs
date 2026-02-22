66d0b86b98b4f9922243bd44ab33d55a4a9ffe1a
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-02-23 06:20:35 +0700

    Adding Sorting, Filtering, Searching and Pagination - Sorting Functionality to  the API

--- NetApiRestore/Controllers/ProductsController.cs
+++ NetApiRestore/Controllers/ProductsController.cs
@@ -3,15 +3,30 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NetApiRestore.Data;
 using NetApiRestore.Entities;
+using NetApiRestore.Extensions;
 
 namespace NetApiRestore.Controllers
 {
     public class ProductsController(StoreContext context) : BaseApiController
 	{
         [HttpGet]
-		public async Task<ActionResult<List<Product>>> GetProducts()
+		public async Task<ActionResult<List<Product>>> GetProducts(string orderBy)
 		{
-			return await context.Products.AsNoTracking().ToListAsync();
+			//var query = context.Products.AsQueryable();
+
+			//query = orderBy switch
+			//{
+			//	"price" => query.OrderBy(x => x.Price),
+			//	"priceDesc" => query.OrderByDescending(x => x.Price),
+			//	_ => query.OrderBy(x => x.Name)
+			//};
+
+			// custom with extensions
+			var query = context.Products
+				.Sort(orderBy)
+				.AsQueryable();
+
+			return await query.ToListAsync();
 		}
 
 		[HttpGet("{id}")]
---
+++ NetApiRestore/Extensions/ProductExtensions.cs
@@ -0,0 +1,19 @@
+﻿using NetApiRestore.Entities;
+
+namespace NetApiRestore.Extensions
+{
+	public static class ProductExtensions
+	{
+		public static IQueryable<Product> Sort(this IQueryable<Product> query, string? orderBy)
+		{
+			query = orderBy switch
+			{
+				"price" => query.OrderBy(x => x.Price),
+				"priceDesc" => query.OrderByDescending(x => x.Price),
+				_ => query.OrderBy(x => x.Name)
+			};
+
+			return query;
+		}
+	}
+}
