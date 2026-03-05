64d8393c50675e8388a6a57fb0f0b92889a185e5
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-03-05 17:16:12 +0700

    Adding the Order Feature - Creating the oders Controller

---
+++ NetApiRestore/Controllers/OrdersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetApiRestore.Data;
+using NetApiRestore.Entities.OrderAggregate;
+using NetApiRestore.Extensions;
+
+namespace NetApiRestore.Controllers
+{
+	[Authorize]
+	public class OrdersController(StoreContext context) : BaseApiController
+	{
+		[HttpGet]
+		public async Task<ActionResult<List<Order>>> GetOrders()
+		{
+			var orders = await context.Orders
+				.Include(x => x.OrderItems)
+				.Where(x => x.BuyerEmail == User.GetUsername())
+				.ToListAsync();
+
+			return orders;
+		}
+
+		[HttpGet("{id:int}")]
+		public async Task<ActionResult<Order>> GetOrderDetails(int id)
+		{
+			var order = await context.Orders
+				.Where(x => x.BuyerEmail == User.GetUsername() && id == x.Id)
+				.FirstOrDefaultAsync();
+
+			if (order == null) return NotFound();
+
+			return order;
+		}
+	}
+}
---
+++ NetApiRestore/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace NetApiRestore.Extensions
+{
+	public static class ClaimsPrincipalExtensions
+	{
+		public static string GetUsername(this ClaimsPrincipal user)
+		{
+			return user.Identity?.Name ?? throw new UnauthorizedAccessException();
+		}
+	}
+}
