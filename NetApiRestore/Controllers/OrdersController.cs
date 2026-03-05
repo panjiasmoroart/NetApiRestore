049cc4723c5c7ea9fcf3882ab8978f003021f5f5
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-03-05 17:30:07 +0700

    Adding the Order Feature - Adding the Create Order logic in the controller and Challenge

--- NetApiRestore/Controllers/OrdersController.cs
+++ NetApiRestore/Controllers/OrdersController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NetApiRestore.Data;
+using NetApiRestore.DTOs;
+using NetApiRestore.Entities;
 using NetApiRestore.Entities.OrderAggregate;
 using NetApiRestore.Extensions;
 
@@ -33,5 +35,54 @@ namespace NetApiRestore.Controllers
 
 			return order;
 		}
+
+		[HttpPost]
+		public async Task<ActionResult<Order>> CreateOrder(CreateOrderDto orderDto)
+		{
+			var basket = await context.Baskets.GetBasketWithItems(Request.Cookies["basketId"]);
+
+			if (basket == null || basket.Items.Count == 0 || string.IsNullOrEmpty(basket.PaymentIntentId))
+				BadRequest("Basket is empty or not found");
+
+			var items = CreateOrderItems(basket.Items);
+			if (items == null) return BadRequest("Some items out of stock");
+
+			var subtotal = items.Sum(x => x.Price * x.Quantity);
+			var deliveryFee = CalculateDeliveryFee(subtotal);
+			long discount = 0;
+
+			var order = new Order
+			{
+				OrderItems = items,
+				BuyerEmail = User.GetUsername(),
+				ShippingAddress = orderDto.ShippingAddress,
+				DeliveryFee = deliveryFee,
+				Subtotal = subtotal,
+				Discount = discount,
+				PaymentSummary = orderDto.PaymentSummary,
+				PaymentIntentId = basket.PaymentIntentId
+			};
+
+			context.Orders.Add(order);
+
+			context.Baskets.Remove(basket);
+			Response.Cookies.Delete("basketId");
+
+			var result = await context.SaveChangesAsync() > 0;
+			if (!result) return BadRequest("Problem creating order");
+
+			return CreatedAtAction(nameof(GetOrderDetails), new { id = order.Id }, order);
+		}
+
+		private long CalculateDeliveryFee(long subtotal)
+		{
+			return subtotal > 10000 ? 0 : 500;
+		}
+
+		private List<OrderItem>? CreateOrderItems(List<BasketItem> items)
+		{
+			throw new NotImplementedException();
+		}
+
 	}
 }
---
+++ NetApiRestore/DTOs/CreateOrderDto.cs
@@ -0,0 +1,10 @@
+﻿using NetApiRestore.Entities.OrderAggregate;
+
+namespace NetApiRestore.DTOs
+{
+	public class CreateOrderDto
+	{
+		public required ShippingAddress ShippingAddress { get; set; }
+		public required PaymentSummary PaymentSummary { get; set; }
+	}
+}
